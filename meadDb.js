cd6119bf1910795273f95e9b718ae643ab8e96e6
MohonSaha <practicewithmohon@gmail.com>
2023-02-26 09:07:07 +0600

    async await example with try catch

--- meadDb.js
+++ meadDb.js
@@ -59,12 +59,29 @@ const searchMeals = () => {
 
 
 // async await example: 
-const loadMealDetail = async (mealId) => {
+/* const loadMealDetail = async (mealId) => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
     const res = await fetch(url);
     const data = await res.json();
     displayMealDetails(data.meals[0]);   
 }
+*/
+
+// async await example with try catch:
+// We use try catch to handle fetch error and data load problem.
+const loadMealDetail = async (mealId) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+
+    try{
+        const res = await fetch(url);
+    const data = await res.json();
+    displayMealDetails(data.meals[0]); 
+    } 
+    catch(error){
+        console.log(error);
+    } 
+}
+
 
 
 const displayMealDetails = (meal) => {
